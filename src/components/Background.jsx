633c281e52dd04b68672c0a8057b1d42a1019f14
Krunal022 <krunalvaghela723@gmail.com>
2025-08-30 19:32:14 +0530

    background styles using Tailwind

---
+++ src/components/Background.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Background = () => {
+  return (
+    <div className="w-full h-screen bg-zinc-800">
+      <h3 className="text-md w-full h-35 text-zinc-400 flex items-center justify-center mt-0.5">
+        Documents
+      </h3>
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[18vw] font-semibold text-zinc-900">
+        DOCS
+      </h1>
+    </div>
+  );
+};
+
+export default Background;
