3db38ae551c186dea842101f0f24da973a5c6fbe
Krunal022 <krunalvaghela723@gmail.com>
2025-08-31 18:40:59 +0530

    card view design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { FaRegFileAlt } from "react-icons/fa";
 
 const Card = () => {
-  return <div className="h-80 w-60 rounded-4xl bg-sky-200"></div>;
+  return (
+    <div className="relative overflow-hidden h-80 w-60 rounded-4xl bg-sky-200 flex-wrap shrink-0">
+      <FaRegFileAlt className="font-black absolute right-5 text-xl top-5" />
+      <p className="tracking-tight p-5 leading-none text-lg mt-8 font-medium">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+        exercitationem!
+      </p>
+      <div className="bg-green-400 cursor-pointer absolute bottom-0 w-full h-10 flex items-center justify-center font-black">
+        Complete
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -3,7 +3,10 @@ import Card from "./Card";
 
 const Forground = () => {
   return (
-    <div className="absolute top-0 left-0 w-full h-screen flex gap-5 p-3">
+    <div className="absolute top-0 left-0 w-full h-screen flex gap-5 p-3 overflow-auto">
+      <Card />
+      <Card />
+      <Card />
       <Card />
     </div>
   );
